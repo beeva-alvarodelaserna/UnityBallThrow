dddb9b30063d4b14a475a850d2b6eecd802b4509
alvaro.delaserna <alvaro.delaserna@bbva.com>
2017-03-15 09:49:33 +0100

    initial commit

---
+++ Assets/CollisionTest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class CollisionTest : MonoBehaviour {
+
+	public AudioClip alert;
+	AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		// audio.PlayOneShot(alert, 0.7F);
+		audio.Play();
+	    print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
+	    // print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
+	    // print("Their relative velocity is " + collisionInfo.relativeVelocity);
+	}
+
+	void OnCollisionStay(Collision collisionInfo)
+	{
+	    print(gameObject.name + " and " + collisionInfo.collider.name + " are still colliding");
+	}
+
+	void OnCollisionExit(Collision collisionInfo)
+	{
+	    print(gameObject.name + " and " + collisionInfo.collider.name + " are no longer colliding");
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+	    print("Collision detected with trigger object " + other.name);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+	    print("Still colliding with trigger object " + other.name);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+	    print(gameObject.name + " and trigger object " + other.name + " are no longer colliding");
+	}
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+    
+	private Vector3 screenPoint;
+	private Vector3 offset;
+
+	private Rigidbody rigidbody;
+
+	bool isInPlace = false;
+		
+	void OnMouseUp(){
+		isInPlace = true;
+	}
+
+	void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+		
+	void OnMouseDrag(){
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		transform.position = cursorPosition;
+	}
+
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update () {  
+        if (isInPlace) {
+            rigidbody.useGravity = true;
+        }
+
+         if(gameObject.transform.position.y < 0)
+	     {
+	         Destroy(gameObject);
+	     }
+    }
+}
